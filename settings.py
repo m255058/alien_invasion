c9835b4586843c2c235a347f2667d7ede7bfe277
m255058 <m255058@usna.edu>
2022-10-25 11:37:56 -0400

    Current Game (completed chapter 12)
    Added bullets ensured bullets disappear and limited the amount of bullets on screen at any one time

--- settings.py
+++ settings.py
@@ -5,5 +5,13 @@ class Settings:
         self.screen_width = 800
         self.screen_height = 500
         self.bg_color = (150,0,255)
+
         #ship speed
         self.ship_speed = 1.5
+
+        #Bullet Settings
+        self.bullet_speed = 1.0
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = (60,60,60)
+        self.bullets_allowed = 5
--- ship.py
+++ ship.py
@@ -25,9 +25,9 @@ class Ship:
         '''update the ships position'''
         #update the ships value not rect.x
         if self.moving_right and self.rect.right < self.screen_rect.right:
-            self.rect.x += self.settings.ship_speed
+            self.x += self.settings.ship_speed
         if self.moving_left and self.rect.left > 0:
-            self.rect.x -= self.settings.ship_speed
+            self.x -= self.settings.ship_speed
         #update rect object from self.x
         self.rect.x = self.x
 
